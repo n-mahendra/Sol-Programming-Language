91f1523b625584b3ea4011599cd0b99ecac07627
Mahendra <nmahendra@msn.com>
2025-07-19 11:27:57 +0530

    Add files via upload

---
+++ src/sol.hpp
@@ -0,0 +1,9 @@
+// sol.hpp
+// Sol header files for C++
+// <<extern "C">> not supplied automatically because Sol also compiles as C++
+
+extern "C" {
+#include "sol.h"
+#include "sollib.h"
+#include "lauxlib.h"
+}
